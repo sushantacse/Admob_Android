d4fc5b7600013aa5c97123ae892c9ccfb3ed78f2
Sushanta Paul <sushantapaul.info@gmail.com>
2019-07-18 16:44:31 +0600

    intial commit

---
+++ app/src/main/java/com/cbiu/admob/MainActivity.java
@@ -0,0 +1,26 @@
+package com.cbiu.admob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+    }
+}
